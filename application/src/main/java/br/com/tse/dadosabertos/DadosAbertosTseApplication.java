e64995cf6ce54814c5d4998af0e52f0b04738e9f
Ivan Rodrigues <ivanzito.jr@gmail.com>
2022-11-21 11:40:07 -0300

    feat:configured the prometheus/grafana

--- application/src/main/java/br/com/tse/dadosabertos/DadosAbertosTseApplication.java
+++ application/src/main/java/br/com/tse/dadosabertos/DadosAbertosTseApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableBatchProcessing
